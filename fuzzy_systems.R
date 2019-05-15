f43edc811429065ff2aead0b5b743372e35642cf
Felipe Vitor Souza dos Reis <felipe.vitow@gmail.com>
2019-05-15 19:33:25 -0300

    Alterações e Correções de valores
    Regras consertadas, Temperatura alterada e Inserida parte do código responsável em testar o sistema fuzzy, no entanto, apresenta um erro.

--- fuzzy_systems.R
+++ fuzzy_systems.R
@@ -1,9 +1,9 @@
 library(sets)
-sets_options("universe", seq(1, 100, 0.5))
+sets_options("universe", seq(-10, 100, 0.5))
 
-Temperatura  = fuzzy_variable(Frio       = fuzzy_normal(0, 10),
-                              Medio      = fuzzy_normal(30, 10),
-                              Quente     = fuzzy_normal(60, 10))
+Temperatura  = fuzzy_variable(Frio       = fuzzy_normal(-5, 5),
+                              Medio      = fuzzy_normal(15, 5),
+                              Quente     = fuzzy_normal(35, 5))
 
 Umid_Rel     = fuzzy_variable(Baixo      = fuzzy_cone(12.5, 12.5),
                               Medio      = fuzzy_cone(32.5, 12.5),
@@ -28,10 +28,10 @@ rules = set(
   fuzzy_rule(Umid_Solo %is% Molhado && (Umid_Rel %is% Baixo || Temperatura %is% Quente) , Irr_Duration %is% Baixo),
   
   fuzzy_rule(Umid_Solo %is% Medio && Umid_Rel %is% Baixo, Irr_Duration %is% Longo),
-  fuzzy_rule(Umid_Solo %is% Baixo && Umid_Rel %is% Baixo, Irr_Duration %is% Baixo),
+  fuzzy_rule(Umid_Solo %is% Medio && Umid_Rel %is% Medio, Irr_Duration %is% Baixo),
   fuzzy_rule(Umid_Solo %is% Medio && Umid_Rel %is% Alto, Irr_Duration %is% Baixo),
   fuzzy_rule(Umid_Solo %is% Medio && Temperatura %is% Quente && Umid_Rel %is% Baixo, Irr_Duration %is% MuitoLongo),
-  fuzzy_rule(Umid_Solo %is% Medio && Temperatura %is% Quente && Umid_Rel %is% Medium, Irr_Duration %is% Longo),
+  fuzzy_rule(Umid_Solo %is% Medio && Temperatura %is% Quente && Umid_Rel %is% Medio, Irr_Duration %is% Longo),
 
   fuzzy_rule(Umid_Solo %is% Seco && (Temperatura %is% Quente || Umid_Rel %is% Baixo), Irr_Duration %is% MuitoLongo),
   fuzzy_rule(Umid_Solo %is% Seco && Umid_Rel %is% Medio, Irr_Duration %is% Longo),
@@ -48,7 +48,7 @@ plot(Temperatura)
 plot(Umid_Rel)
 plot(Irr_Duration)
 
-fi <- fuzzy_inference(modelo, list(Temperatura = 30, Umid_Rel = 20, Umid_Solo = 20))
+fi <- fuzzy_inference(modelo, list(Temperatura = 5, Umid_Rel = 7, Umid_Solo = 6.5))
 plot(fi)
 
 a = fi[1][1]
@@ -59,16 +59,16 @@ gset_defuzzify(fi, "centroid")
 abline(v=gset_defuzzify(fi, "centroid"), col="blue")
 
 
-#par(mfrow=c(3,3))
-#result = list(); cont = 0
-#for (i in seq(5, 50, by=5)){
-#  t=45
-#  us= 100
-#  l= 11000
-#  cont = cont+1
-#result[[cont]] <- fuzzy_inference(modelo, list(Temperatura = 50-1*i, Umid_Solo = us-2*i, Luminosidade = l - 230*i))
-#  plot(result[[cont]])
-#  print(paste(v-1*i, us - 2*i, l - 230*i, gset_defuzzify(result[[cont]], "centroid"), sep = " - "))
-#}
+par(mfrow=c(4,3))
+result = list(); cont = 0
+for (i in seq(5, 50, by=5)){
+  temp=50
+  umd_sl= 70
+  umd_rel= 65
+  cont = cont+1
+result[[cont]] <- fuzzy_inference(modelo, list(Temperatura = temp-1*i, Umid_Solo = umd_sl-1.4*i, Umid_Rel = umd_rel - 1.3*i))
+  plot(result[[cont]])
+  print(paste(i,temp-1*i, umd_sl-1.4*i, umd_rel - 1.3*i, gset_defuzzify(result[[cont]], "centroid"), sep = " - "))
+}
 
                         
