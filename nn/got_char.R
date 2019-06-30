debd57688ca4a5c6d3572a75c8fe9efff5c552d7
Pablo Wesley <wesley_pablo68@yahoo.com>
2019-06-30 14:27:55 -0300

    Add got_char class nn

---
+++ nn/got_char.R
@@ -0,0 +1,30 @@
+library(neuralnet)
+
+dataset = read.csv('got_dataset.csv')
+dataset.train = dataset[1:30,]
+dataset.test = dataset [31:nrow(dataset),]
+
+quest.qty = 8
+ans.qty = 4
+
+quest.formula = paste(colnames(dataset[1:quest.qty]),collapse = " + ")
+class.formula = paste(colnames(dataset[(quest.qty+1):length(dataset)]),collapse = " + ")
+
+formula = as.formula(paste(class.formula," ~ ",quest.formula))
+nn.train = neuralnet(formula, data = dataset.train, hidden = c(3,4), threshold = 0.001, linear.output = F)
+nn.test = compute(nn.train,dataset.test[1:quest.qty])
+
+nn.result = data.frame(target = max.col(dataset.test[(quest.qty+1):length(dataset)]),
+                       output = max.col(nn.test$net.result))
+nn.result$error = abs(nn.result$output - nn.result$target)
+
+plot(nn.result$target, col="green", pch=20,xaxt = "none", yaxt="none",
+     main = "Qual personagem de GOT é você?",
+     xlab = "Entradas",ylab = "Personagens")
+points(nn.result$output, col="red")
+axis(1,at=seq(1,nrow(dataset.test)))
+axis(2,at=seq(1,4))
+
+print(nn.train$result.matrix[1:3,])
+print(paste(sum(nn.result$error == 0)," acertos de ",nrow(nn.result)),quote = F)
+print(nn.result)
